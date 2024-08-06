8e8d2ba3b3f002d0d1d3f339779fe80f2138bbfd
Usuario <Usuario@CPD02-M18>
2024-08-06 11:11:41 -0300

    Adicionar arquivos de projeto.

---
+++ CauculadoraFamiliar/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CauculadoraFamiliar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TextoTG = new System.Windows.Forms.Label();
+            this.TextoSaldo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CTenergia = new System.Windows.Forms.TextBox();
+            this.CTagua = new System.Windows.Forms.TextBox();
+            this.CTtv = new System.Windows.Forms.TextBox();
+            this.CTalimentacao = new System.Windows.Forms.TextBox();
+            this.CToutros = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Caucular = new System.Windows.Forms.Button();
+            this.NovoCalculo = new System.Windows.Forms.Button();
+            this.Fechar = new System.Windows.Forms.Button();
+            this.CTrendaf = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Renda Familiar:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(87, 451);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total de Gastos";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(87, 524);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Saldo:";
+            // 
+            // TextoTG
+            // 
+            this.TextoTG.AutoSize = true;
+            this.TextoTG.Location = new System.Drawing.Point(348, 451);
+            this.TextoTG.Name = "TextoTG";
+            this.TextoTG.Size = new System.Drawing.Size(41, 13);
+            this.TextoTG.TabIndex = 9;
+            this.TextoTG.Text = "label10";
+            // 
+            // TextoSaldo
+            // 
+            this.TextoSaldo.AutoSize = true;
+            this.TextoSaldo.Location = new System.Drawing.Point(348, 524);
+            this.TextoSaldo.Name = "TextoSaldo";
+            this.TextoSaldo.Size = new System.Drawing.Size(41, 13);
+            this.TextoSaldo.TabIndex = 10;
+            this.TextoSaldo.Text = "label11";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CTenergia);
+            this.groupBox1.Controls.Add(this.CTagua);
+            this.groupBox1.Controls.Add(this.CTtv);
+            this.groupBox1.Controls.Add(this.CTalimentacao);
+            this.groupBox1.Controls.Add(this.CToutros);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(63, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 228);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gastos:";
+            // 
+            // CTenergia
+            // 
+            this.CTenergia.Location = new System.Drawing.Point(110, 51);
+            this.CTenergia.Name = "CTenergia";
+            this.CTenergia.Size = new System.Drawing.Size(203, 20);
+            this.CTenergia.TabIndex = 12;
+            // 
+            // CTagua
+            // 
+            this.CTagua.Location = new System.Drawing.Point(110, 86);
+            this.CTagua.Name = "CTagua";
+            this.CTagua.Size = new System.Drawing.Size(203, 20);
+            this.CTagua.TabIndex = 11;
+            // 
+            // CTtv
+            // 
+            this.CTtv.Location = new System.Drawing.Point(110, 117);
+            this.CTtv.Name = "CTtv";
+            this.CTtv.Size = new System.Drawing.Size(203, 20);
+            this.CTtv.TabIndex = 10;
+            // 
+            // CTalimentacao
+            // 
+            this.CTalimentacao.Location = new System.Drawing.Point(109, 150);
+            this.CTalimentacao.Name = "CTalimentacao";
+            this.CTalimentacao.Size = new System.Drawing.Size(203, 20);
+            this.CTalimentacao.TabIndex = 9;
+            // 
+            // CToutros
+            // 
+            this.CToutros.Location = new System.Drawing.Point(109, 180);
+            this.CToutros.Name = "CToutros";
+            this.CToutros.Size = new System.Drawing.Size(203, 20);
+            this.CToutros.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Energia Elétrica:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Água:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "TV/Fone/Internet:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Alimentação:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Outros Gastos:";
+            // 
+            // Caucular
+            // 
+            this.Caucular.Location = new System.Drawing.Point(92, 346);
+            this.Caucular.Name = "Caucular";
+            this.Caucular.Size = new System.Drawing.Size(75, 23);
+            this.Caucular.TabIndex = 12;
+            this.Caucular.Text = "Caucular";
+            this.Caucular.UseVisualStyleBackColor = true;
+            this.Caucular.Click += new System.EventHandler(this.Caucular_Click);
+            // 
+            // NovoCalculo
+            // 
+            this.NovoCalculo.Location = new System.Drawing.Point(196, 346);
+            this.NovoCalculo.Name = "NovoCalculo";
+            this.NovoCalculo.Size = new System.Drawing.Size(75, 23);
+            this.NovoCalculo.TabIndex = 13;
+            this.NovoCalculo.Text = "Novo";
+            this.NovoCalculo.UseVisualStyleBackColor = true;
+            this.NovoCalculo.Click += new System.EventHandler(this.NovoCalculo_Click);
+            // 
+            // Fechar
+            // 
+            this.Fechar.Location = new System.Drawing.Point(301, 346);
+            this.Fechar.Name = "Fechar";
+            this.Fechar.Size = new System.Drawing.Size(75, 23);
+            this.Fechar.TabIndex = 14;
+            this.Fechar.Text = "Fechar";
+            this.Fechar.UseVisualStyleBackColor = true;
+            this.Fechar.Click += new System.EventHandler(this.Fechar_Click);
+            // 
+            // CTrendaf
+            // 
+            this.CTrendaf.Location = new System.Drawing.Point(146, 46);
+            this.CTrendaf.Name = "CTrendaf";
+            this.CTrendaf.Size = new System.Drawing.Size(203, 20);
+            this.CTrendaf.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 694);
+            this.Controls.Add(this.CTrendaf);
+            this.Controls.Add(this.Fechar);
+            this.Controls.Add(this.NovoCalculo);
+            this.Controls.Add(this.Caucular);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TextoSaldo);
+            this.Controls.Add(this.TextoTG);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gastos:";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label TextoTG;
+        private System.Windows.Forms.Label TextoSaldo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox CTenergia;
+        private System.Windows.Forms.TextBox CTagua;
+        private System.Windows.Forms.TextBox CTtv;
+        private System.Windows.Forms.TextBox CTalimentacao;
+        private System.Windows.Forms.TextBox CToutros;
+        private System.Windows.Forms.Button Caucular;
+        private System.Windows.Forms.Button NovoCalculo;
+        private System.Windows.Forms.Button Fechar;
+        private System.Windows.Forms.TextBox CTrendaf;
+    }
+}
+
---
+++ CauculadoraFamiliar/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CauculadoraFamiliar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Caucular_Click(object sender, EventArgs e)
+        {
+            double GT = int.Parse(CTenergia.Text);
+            GT += int.Parse(CTagua.Text);
+            GT += int.Parse(CTtv.Text);
+            GT += int.Parse(CTalimentacao.Text);
+            GT += int.Parse(CToutros.Text);
+
+            TextoTG.Text = GT.ToString();
+
+            double RF = int.Parse(CTrendaf.Text);
+
+            TextoSaldo.Text = (RF-GT).ToString();
+        }
+
+        private void NovoCalculo_Click(object sender, EventArgs e)
+        {
+            CTrendaf.Text = " ";
+            CTenergia.Text = " ";
+            CTagua.Text = " ";
+            CTtv.Text = " ";
+            CTalimentacao.Text = " ";
+            CToutros.Text = " ";
+            TextoTG.Text = " ";
+            TextoSaldo.Text = " ";
+
+            CTrendaf.Focus();
+        }
+
+        private void Fechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
